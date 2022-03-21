4baeebfcb395802261b3867c7e0a5ce6fa782e3b
Dayane Núbia <dayane.nubia67@gmail.com>
2022-03-21 19:46:40 -0300

    Update alocacaoDinamicaMemoriaEx.c

--- alocacaoDinamicaMemoriaEx.c
+++ alocacaoDinamicaMemoriaEx.c
@@ -2,14 +2,14 @@
 #include<stdlib.h>
 
 int main(){
-	int n,i;
-	int *v;
+	int n,i; //declarando variaveis
+	int *v;  // declarando ponteiro
 	printf("Tamanho do vetor:");
-	scanf("%d", &n);
-	v = malloc(sizeof(int)*n);
+	scanf("%d", &n);  //recebendo o tamanho do vetor 
+	v = malloc(sizeof(int)*n);  //
 	for(i = 0; i < n; i++){
 		v[i] = i * 2;
 		printf("%d|", v[i]);
-	}
+	}  //fim for 
 	return 0;
 }
